ef961352415d713b3c34744f6195a125c6d41df4
cmg218 <cmg218@lehigh.edu>
2015-08-28 16:05:01 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,22 @@
+// Christine Gelotte
+// August 28 2015
+// Lab 1 Hello World Program
+
+
+
+// define a class
+public class HelloWorld {
+    // add main method method
+    public static void main(String[] args){
+        // print statement with box around it
+        System.out.println("#####################");
+        System.out.println("#####################");
+        System.out.println("#                   #");
+        System.out.println("# Hello World!!!!!!!#");
+        System.out.println("#                   #");
+        System.out.println("#####################");
+        System.out.println("#####################");
+        System.out.println("test");
+        //test/testtest
+    }
+}
