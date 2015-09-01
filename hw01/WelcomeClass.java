037fa81f186fb265bb59736cbc655dd1ca7a1f20
cmg218 <cmg218@lehigh.edu>
2015-09-01 00:07:32 +0000

    homework 1

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,20 @@
+//Christine Gelotte
+// Homework 1 -- Welcome class 
+//Due: September 2, 2015
+
+//define a class
+public class WelcomeClass {
+    //add main method
+    public static void main(String[] args){
+        // print welcome class and tweet about self
+        System.out.println("  -----------");
+        System.out.println("  | WELCOME |");
+        System.out.println("  -----------");
+        System.out.println("  ^  ^  ^  ^  ^  ^  ");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+        System.out.println(" <-C--M--G--2--1--8-> ");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ / ");
+        System.out.println("  v  v  v  v  v  v ");
+        System.out.println("I love nature and spending time outside. When I see something beautiful or interesting I take a picture of it.");
+    }
+}
